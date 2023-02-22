94d72b66f9f30fdcbdc4374c76d8ad21cdf8901f
Isaac van Oosterom <isaac.vanoosterom@gmail.com>
2023-02-22 15:50:11 -0600

    WIP Still working on .drive()

--- src/main/java/frc/robot/WindChillSwerveModule.java
+++ src/main/java/frc/robot/WindChillSwerveModule.java
@@ -37,10 +37,39 @@ public class WindChillSwerveModule {
     public void setDesiredState(SwerveModuleState desiredState) {
         // Custom optimize command, since default WPILib optimize assumes continuous controller which
         // REV and CTRE are not
-        // desiredState = OnboardModuleState.optimize(desiredState, getState().angle);
+        desiredState = desiredState.optimize(desiredState, getState().angle);
     
-        // setAngle(desiredState);
-        // setSpeed(desiredState, isOpenLoop);
+        setAngle(desiredState);
+        setSpeed(desiredState);
     }
+
+    private void setSpeed(SwerveModuleState desiredState) {
+          double percentOutput = desiredState.speedMetersPerSecond / Constants.maxSpeed;
+          driveMotor.set(percentOutput);
+      }
+    
+      private void setAngle(SwerveModuleState desiredState) {
+        // TODO PID stuff is very involved here, not sure how to remove it
+        // Prevent rotating module if speed is less then 1%. Prevents jittering.
+        // Rotation2d angle =
+        //     (Math.abs(desiredState.speedMetersPerSecond) <= (Constants.Swerve.maxSpeed * 0.01))
+        //         ? lastAngle
+        //         : desiredState.angle;
+    
+        // angleController.setReference(angle.getDegrees(), ControlType.kPosition);
+        // lastAngle = angle;
+      }
+
+      private Rotation2d getAngle() {
+        return Rotation2d.fromDegrees(integratedAngleEncoder.getPosition());
+      }
+    
+      public Rotation2d getCanCoder() {
+        return Rotation2d.fromDegrees(angleEncoder.getAbsolutePosition());
+      }
+
+      public SwerveModuleState getState() {
+        return new SwerveModuleState(driveEncoder.getVelocity(), getAngle());
+      }
 }
 
